83239842115f78ed9a24df8185e8b2391ee35947
NinjaJc01 <james@cookiehome.co.uk>
2020-07-12 03:03:55 +0100

    Improved resources

--- resources/main.js
+++ resources/main.js
@@ -1,18 +1,1 @@
 console.log("Hello, World!")
-async function getData(url = '') {
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *client
-    });
-    return await response.text();
-}
-
-async function onPageLoad() {
-    const myIp = await getData("/whoami");
-    console.log(myIp);
-    document.getElementById("myIp").textContent = myIp;
-}
